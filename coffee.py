185879b9a81179d2d2692133a7721108fd29b18a
moreen karimi <moreenk541@gmail.com>
2025-05-28 15:25:03 +0300

    code revision

--- coffee.py
+++ coffee.py
@@ -17,17 +17,17 @@ class Coffee:
 
 
     def orders(self):
-        return(order for order in Order.all if order.coffee == self)
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
         return list({order.customer for order in self.orders()})
 
     def num_orders(self):
-        return len(self.orders()) 
+        return len(self.orders())
 
     def average_price(self):
         orders = self.orders()
         if not orders:
             return 0
         total = sum(order.price for order in orders)
-        return total / len(orders)     
+        return total / len(orders)
